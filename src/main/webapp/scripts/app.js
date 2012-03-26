7486dd629d955744b1cf665d34971e336ef256bc
Brian Gray <bgray@summa-tech.com>
2012-03-26 18:14:40 -0400

    Merged master with steal stuff

--- src/main/webapp/scripts/app.js
+++
@@ -1,4 +0,0 @@
-(function(){
-	// called once jquery loads
-})();
-
--- src/main/webapp/scripts/html5player.js
+++ src/main/webapp/scripts/html5player.js
@@ -1,9 +1,8 @@
-steal(  'https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js',
+steal(  '../lib/jquery',
         'https://ajax.googleapis.com/ajax/libs/jqueryui/1.8.18/jquery-ui.min.js',
         '../lib/modernizr.js',
-        'app' )
+        'main' )
 	.css( '../style/html5player' )		        // Loads 'html5player.css'
 	.then(function(){						// Adds a function to be called back once all prior files have been loaded and run 
         // do nothing
 	});
-	 
